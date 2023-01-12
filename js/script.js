d91ee7d29ead032e195906b915dd24a2e6c35efb
Yevheniia Maksymiv <evgeniamaksymiv@gmail.com>
2023-01-12 17:16:46 +0200

    added buttons

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const select2 = document.getElementById('select-2');
 const input = document.getElementById('input');
 const btnConvert = document.getElementById('btn-convert');
 const resultList = document.getElementById('result-list');
+const btnHistory = document.getElementById('btn-history');
+const btnClearHistory = document.getElementById('btn-clear-history');
 
 const date = new Date().toLocaleDateString();
 currentDate.innerHTML = `Current rate for ${date}:`;
@@ -63,33 +65,35 @@ select2.onchange = () => {
   currencyTo = select2.options[select2.selectedIndex].text;
 };
 
-const addItemToList = (amount, currency1, currency2, result) => {
-  const id = Math.random().toString(16).slice(2);
-  const liContent = `${amount} ${currency1} = ${result} ${currency2}`;
-  localStorage.setItem(`${id}`, `${liContent}`);
-  // console.log(Object.entries(localStorage))
-  // Object.entries(localStorage).forEach(el => {
-  //   const li = document.createElement('li');
-  //   li.id = +el[0];
-  //   li.innerHTML = el[1];
-  //   const icon = document.createElement('span');
-  //   icon.id = li.id;
-  //   icon.className = 'material-icons';
-  //   icon.classList.add('icon-delete');
-  //   icon.appendChild(document.createTextNode('cancel'));
-  //   li.appendChild(icon);
-  //   resultList.appendChild(li);
-  // })
+const createListItem = (id) => {
   const li = document.createElement('li');
   li.id = id;
   li.innerHTML = localStorage.getItem(`${id}`);
+
+  const btnDeleteItem = document.createElement('button');
+  btnDeleteItem.style.backgroundColor = '#121212';
+  btnDeleteItem.style.border = 'none';
+  btnDeleteItem.id = li.id;
+
   const icon = document.createElement('span');
-  icon.id = li.id;
   icon.className = 'material-icons';
   icon.classList.add('icon-delete');
   icon.appendChild(document.createTextNode('cancel'));
-  li.appendChild(icon);
+  btnDeleteItem.appendChild(icon);
+  li.appendChild(btnDeleteItem);
   resultList.appendChild(li);
+
+  btnDeleteItem.onclick = () => {
+    li.innerHTML = '';
+  }
+}
+
+const addItemToList = (amount, currency1, currency2, result) => {
+
+  const uniqueId = Math.random().toString(16).slice(2);
+  const liContent = `${amount} ${currency1} = ${result} ${currency2}`;
+  localStorage.setItem(`${uniqueId}`, `${liContent}`);
+  createListItem(uniqueId);
 };
 
 btnConvert.onclick = () => {
@@ -100,3 +104,16 @@ btnConvert.onclick = () => {
     input.value = '';
   }
 };
+
+btnHistory.onclick = () => {
+  resultList.innerHTML = '';
+  Object.keys(localStorage).forEach((id) => createListItem(id));
+  btnClearHistory.innerHTML = 'Clear All';
+  btnClearHistory.className = 'btn btn-danger';
+}
+
+btnClearHistory.onclick = () => {
+  localStorage.clear();
+  resultList.innerHTML = '';
+  // btnClearHistory.remove();
+}
